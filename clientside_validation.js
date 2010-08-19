a5042affa62d0ef1a13284b568e4c394b471f884
Peter Droogmans <peter@attiks.com>
2010-08-19 19:47:21 +0000

    No more inline javascript, all is handled by Drupal.behaviors, main benefit is that it supports ahah operations

--- clientside_validation.js
+++ clientside_validation.js
@@ -1,4 +1,62 @@
-$(document).ready(function() {
+Drupal.behaviors.clientsideValidation = function (context) {
+  if (!Drupal.myClientsideValidation) {
+    Drupal.myClientsideValidation = new Drupal.clientsideValidation();
+  }
+  else {
+    Drupal.myClientsideValidation.bindForms();
+  }
+}
+
+Drupal.clientsideValidation = function() {
+  var self = this;
+  this.prefix = 'clientsidevalidation-';
+  this.data = Drupal.settings.clientsideValidation;
+  this.forms = this.data['forms'];
+  this.validators = {};
+  this.addExtraRules();
+  this.bindForms();
+};
+
+Drupal.clientsideValidation.prototype.bindForms = function(){
+  var self = this;
+  jQuery.each (self.forms, function(f) {
+    // Add error container above the form
+    var errorel = self.prefix + f + '-errors';
+    if (!$('#' + errorel).length) {
+      $('<div id="' + errorel + '" class="messages error clientside-error"><ul></ul></div>').insertBefore('#' + f).hide();
+    }
+    
+    // Remove any existing validation stuff
+    if (self.validators[f]) {
+      // Doesn't work :: $('#' + f).rules('remove');
+      var form = $('#' + f).get(0); 
+      jQuery.removeData(form, 'validator');
+    }
+    
+    // Add basic settings
+    self.validators[f] = $('#' + f).validate({
+      errorClass: 'error',
+      errorContainer: '#' + errorel,
+      errorLabelContainer: '#' + errorel + ' ul',
+      wrapper: 'li'
+    });
+
+    // Bind all rules
+    self.bindRules(f);
+  });
+}
+
+Drupal.clientsideValidation.prototype.bindRules = function(formid){
+  var self = this;
+  jQuery.each (self.forms[formid]['rules'], function(r) {
+    // Check if element exist in DOM before adding the rule
+    if ($("#" + formid + " :input[name='" + r + "']").length) {
+      $("#" + formid + " :input[name='" + r + "']").rules("add", self.forms[formid]['rules'][r]);
+    }
+  });
+}
+
+Drupal.clientsideValidation.prototype.addExtraRules = function(){
 
   // Min a and maximum b checkboxes from a group
   jQuery.validator.addMethod("checkboxgroupminmax", function(value, element, param) { 
@@ -19,4 +77,4 @@ $(document).ready(function() {
     return this.optional(element) || /^-?\d+$/.test(value);
   }, jQuery.format('Please enter only digits.'));
     
-});
+}

d23eb80258731c8285f7517bb9e0415a50329c45
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2011-08-09 12:05:02 +0200

    added and improved html5 number support

--- clientside_validation_html5/clientside_validation_html5.js
+++ clientside_validation_html5/clientside_validation_html5.js
@@ -2,10 +2,32 @@
   Drupal.behaviors.clientsideValidationHtml5 = {
     attach: function (context) {
       $(document).bind('clientsideValidationAddCustomRules', function(event){
-        jQuery.validator.addMethod("Html5Number", function(value, element, param) {
+        jQuery.validator.addMethod("Html5Min", function(value, element, param) {
+          //param[0] = min, param[1] = step;
+          var mismatch = 0;
+          if (param[1] != 'any') {
+            mismatch = (value-param[0])%param[1]
+          }
+          return this.optional(element) || (mismatch == 0 && value >= param[0]);
+        }, jQuery.format('Value must be greater than {0} with steps of {1}.'));
 
-          
-        }, jQuery.format('Please enter only digits.'));
+        jQuery.validator.addMethod("Html5Max", function(value, element, param) {
+          //param[0] = max, param[1] = step;
+          var mismatch = 0;
+          if (param[1] != 'any') {
+            mismatch = (value)%param[1]
+          }
+          return this.optional(element) || (mismatch == 0 && value <= param[0]);
+        }, jQuery.format('Value must be smaller than {0} and must be dividable by {1}.'));
+
+        jQuery.validator.addMethod("Html5Range", function(value, element, param) {
+          //param[0] = min, param[1] = max, param[2] = step;
+          var mismatch = 0;
+          if (param[2] != 'any') {
+            mismatch = (value-param[0])%param[2]
+          }
+          return this.optional(element) || (mismatch == 0 && value >= param[0] && value <= param[1]);
+        }, jQuery.format('Value must be greater than {0} with steps of {2} and smaller than {1}.'));
       });
     }
   }

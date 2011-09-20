d81d73e1d8524523cc34ec829e18d9c07401e348
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2011-09-20 12:43:40 +0200

    small js api changes

--- clientside_validation.api.js
+++ clientside_validation.api.js
@@ -9,24 +9,19 @@
  * equivalent of your custom php validation rule and make it available for
  * Clientside validation. Below is an example of how you would do this.
  */
-//jQuery wrapper
-(function ($) {
-  //Define a Drupal behaviour with a custom name
-  Drupal.behaviors.myModuleBehavior = {
-    attach: function (context) {
-      //Add an eventlistener to the document reacting on the
-      //'clientsideValidationAddCustomRules' event.
-      $(document).bind('clientsideValidationAddCustomRules', function(event){
-        //Add your custom method with the 'addMethod' function of jQuery.validator
-        //http://docs.jquery.com/Plugins/Validation/Validator/addMethod#namemethodmessage
-        jQuery.validator.addMethod("myCustomMethod", function(value, element, param) {
-          //let an element match an exact value defined by the user
-          return value == param;
-          //Enter a default error message, numbers between {} will be replaced
-          //with the matching value of that key in the param array, enter {0} if
-          //param is a value and not an array.
-        }, jQuery.format('Value must be equal to {0}'));
-      });
-    }
-  }
-})(jQuery);
+//Define a Drupal behaviour with a custom name
+Drupal.behaviors.myModuleBehavior = function (context) {
+  //Add an eventlistener to the document reacting on the
+  //'clientsideValidationAddCustomRules' event.
+  $(document).bind('clientsideValidationAddCustomRules', function(event){
+    //Add your custom method with the 'addMethod' function of jQuery.validator
+    //http://docs.jquery.com/Plugins/Validation/Validator/addMethod#namemethodmessage
+    jQuery.validator.addMethod("myCustomMethod", function(value, element, param) {
+      //let an element match an exact value defined by the user
+      return value == param;
+    //Enter a default error message, numbers between {} will be replaced
+    //with the matching value of that key in the param array, enter {0} if
+    //param is a value and not an array.
+    }, jQuery.format('Value must be equal to {0}'));
+  });
+}

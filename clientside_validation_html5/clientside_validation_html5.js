7acccbf8950da7cdd76d7fa5aa41b31916ee2458
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2012-03-09 17:03:05 +0100

    improved html5 number support

--- clientside_validation_html5/clientside_validation_html5.js
+++ clientside_validation_html5/clientside_validation_html5.js
@@ -3,30 +3,60 @@
     attach: function (context) {
       $(document).bind('clientsideValidationAddCustomRules', function(event){
         jQuery.validator.addMethod("Html5Min", function(value, element, param) {
+          var exp = 0;
+          if (value.indexOf('.')) {
+            exp = value.length - value.indexOf('.') - 1;
+          }
+          var multiplier = Math.pow(10, exp);
+
+          value = parseInt(parseFloat(value) * multiplier);
+          var min = parseInt(parseFloat(param[0]) * multiplier);
+
           //param[0] = min, param[1] = step;
           var mismatch = 0;
           if (param[1] != 'any') {
-            mismatch = (value-param[0])%param[1]
+            var step = parseInt(parseFloat(param[1]) * multiplier);
+            mismatch = (value-min)%step
           }
-          return this.optional(element) || (mismatch == 0 && value >= param[0]);
+          return this.optional(element) || (mismatch == 0 && value >= min);
         }, jQuery.format('Value must be greater than {0} with steps of {1}.'));
 
         jQuery.validator.addMethod("Html5Max", function(value, element, param) {
+          var exp = 0;
+          if (value.indexOf('.')) {
+            exp = value.length - value.indexOf('.') - 1;
+          }
+          var multiplier = Math.pow(10, exp);
+
+          value = parseInt(parseFloat(value) * multiplier);
+          var max = parseInt(parseFloat(param[0]) * multiplier);
+
           //param[0] = max, param[1] = step;
           var mismatch = 0;
           if (param[1] != 'any') {
-            mismatch = (value)%param[1]
+            var step = parseInt(parseFloat(param[1]) * multiplier);
+            mismatch = (value)%step
           }
-          return this.optional(element) || (mismatch == 0 && value <= param[0]);
+          return this.optional(element) || (mismatch == 0 && value <= max);
         }, jQuery.format('Value must be smaller than {0} and must be dividable by {1}.'));
 
         jQuery.validator.addMethod("Html5Range", function(value, element, param) {
-          //param[0] = min, param[1] = max, param[2] = step;
+          var exp = 0;
+          if (value.indexOf('.')) {
+            exp = value.length - value.indexOf('.') - 1;
+          }
+          var multiplier = Math.pow(10, exp);
+
+          value = parseInt(parseFloat(value) * multiplier);
+          var min = parseInt(parseFloat(param[0]) * multiplier);
+          var max = parseInt(parseFloat(param[1]) * multiplier);
+          
           var mismatch = 0;
           if (param[2] != 'any') {
-            mismatch = (value-param[0])%param[2]
+            var step = parseInt(parseFloat(param[2]) * multiplier);
+            mismatch = (value-min)%step;
           }
-          return this.optional(element) || (mismatch == 0 && value >= param[0] && value <= param[1]);
+          return this.optional(element) || (mismatch == 0 && value >= min && value <= max);
         }, jQuery.format('Value must be greater than {0} with steps of {2} and smaller than {1}.'));
 
         jQuery.validator.addMethod("Html5Color", function(value, element, param) {
